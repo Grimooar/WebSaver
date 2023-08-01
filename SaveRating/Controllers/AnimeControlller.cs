c810a0388dba2aaf9f5b70a250c1e7ba086afe54
Grimooar <57305965+Grimooar@users.noreply.github.com>
2023-08-01 17:32:28 +0200

    Trying add documentation

--- SaveRating/Controllers/AnimeControlller.cs
+++ SaveRating/Controllers/AnimeControlller.cs
@@ -2,12 +2,14 @@ using WebApplication1.Service;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Swashbuckle.AspNetCore.Annotations;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     [ApiController]
     [Authorize]
-    [Route("api/[controller]")]
+    [Route("api/Anime")]
     public class AnimeController : ControllerBase
     {
         private readonly MyAnimeListService _animeService;
@@ -17,7 +19,15 @@ namespace WebApplication1.Controllers
             _animeService = animeService;
         }
 
+        /// <summary>
+        /// Get list of anime by name 
+        /// </summary>
+        /// <param name="animeName">Anime name for serach.</param>
+        /// <returns>list of anime that contains provided name .</returns>
         [HttpGet]
+        [ProducesResponseType(typeof(List<Anime>), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        [SwaggerOperation(Summary = "Получение списка аниме по названию.")]
         public async Task<IActionResult> GetAnime(string animeName)
         {
             if (string.IsNullOrWhiteSpace(animeName))
--- SaveRating/Program.cs
+++ SaveRating/Program.cs
@@ -117,6 +117,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+});
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
